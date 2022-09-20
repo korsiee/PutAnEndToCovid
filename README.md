2765e4da3b4a2aa87708b396da3f4c130f6dbcdd
korsiee <113998018+korsiee@users.noreply.github.com>
2022-09-20 15:53:06 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+"# streamlit-to-heroku-tutorial" 
+"# PutAnEndToCovid" 
+"# streamlit-to-heroku-tutorial" 
---
+++ Streamlit.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("WELCOME TO KORSIEES SECRET WEBPAGE")
+
+my_text = st.text("A day above ground is a good day")
+
+my_button = st.button("Click here to end covid")
+
+if my_button:
+    st.title("God is putting an end to covid, thanks in advance for your patience.")
